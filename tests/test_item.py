7cfd64239b96d39663ba4c1a4707d7b46ee2663f
valery <1010_29@mail.ru>
2023-06-13 14:26:31 +0400

    add class and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def test_item():
+    return Item("apple_watch", 2300, 5)
+
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 11500
+
+
+def test_apply_discount(test_item):
+    Item.pay_rate = 0.5
+    test_item.apply_discount()
+    assert test_item.price == 1150
